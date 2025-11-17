1ff30937c60c12f1d3d2e76e8f01e5e66619a7d1
Nisha Yadav <Nisha Yadav>
2025-11-17 13:38:44 +0530

    Deploy frontend solve issue commit

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import { Loader } from "lucide-react";
 import Login from "./Login";
