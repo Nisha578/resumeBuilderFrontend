9d2694b309b6591f607f235bbb477f72713f2b9d
Nisha Yadav <Nisha Yadav>
2025-11-17 15:40:45 +0530

    final commit

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -109,8 +109,8 @@ const ResumeBuilder = () => {
   }
 
   const handleShare = () => {
-    // const frontendUrl = window.location.href.split('/app/')[0];
-    const frontendUrl = window.location.origin;
+    const frontendUrl = window.location.href.split('/app/')[0];
+    // const frontendUrl = window.location.origin;
     const resumeUrl = frontendUrl + '/view/' +resumeId;
 
     if(navigator.share) {
